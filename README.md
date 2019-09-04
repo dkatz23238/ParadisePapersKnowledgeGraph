5cfec72ed7a8f18540dfc53b38eea8d74dcb54bd
David Katz <davidemmanuelkatz@gmail.com>
2019-09-04 12:28:27 -0300

    new queries and migration script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Knowledge Graphs to Analyze Tax Evasion
+
+<i> Using GRAKN.AI and the Paradise Papers Leak dataset from the ICIJ </i>
+
+In this repository we follow the common steps in modeling, migrating, and exploring highly conected data observations using `GRAKN` and `GRAQL` the query language. Although the software has originally been developed for bioinformatics; there are a wide range of use cases that can be tackled, problems that can be solved, and questions can be ansewered using the stack of GRAKN.AI tools. My background is in Law and Economics so in this repostiroy I will be trying to visualize and answer some common questions that can be conducted with regards to the Paradise Papers Leak dataset and the general nature of Tax Evasion of corporate elite.
---
+++ migraterels.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from grakn.client import GraknClient
+
+
+def clean_table(df):
+    df["n.name"] = df["n.name"].str.replace('"', '')
+    return df
+
+
+def relationship_template(rel):
+    '''Creates gql relationship insert'''
+    insert_gql = "match $n1 isa thing, has node-id " + rel["node_1"] + ";"
+    insert_gql += "$n2 isa thing, has node-id " + rel["node_2"] + ";"
+    return insert_gql
+
+
+REL_TYPES = [
+    'registered_address', 'officer_of', 'connected_to', 'intermediary_of',
+    'same_name_as', 'same_id_as'
+]
+
+relationships_df = pd.read_csv("./data/paradise_papers.edges.csv").astype(str)
+
+with GraknClient(uri='127.0.0.1:48555') as client:
+    with client.session(keyspace="paradise") as session:
+        for rel_type in REL_TYPES:
+
+            df = relationships_df[relationships_df["rel_type"] == rel_type]
+
+            if rel_type == "connected_to":
+                for row in df.iterrows():
+                    with session.transaction().write() as transaction:
+                        rel = row[1]
+                        query = relationship_template(rel)
+                        query += "insert $connection (x: $n1, y: $n2) isa connection;"
+                        print("Inserting Query:")
+                        print(query)
+                        transaction.query(query)
+                        transaction.commit()
+                        print("\nInsert Complete!\n")
+
+            if rel_type == "intermediary_of":
+                for row in df.iterrows():
+                    with session.transaction().write() as transaction:
+                        rel = row[1]
+                        query = relationship_template(rel)
+                        query += "insert $intermediaryship (x: $n1, y: $n2) isa intermediaryship;"
+                        print("Inserting Query:")
+                        print(query)
+                        transaction.query(query)
+                        transaction.commit()
+                        print("\nInsert Complete!\n")
